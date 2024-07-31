97b00669a3dada2e26f20a0c062f88edea7bf633
Amayuru1999 <amarasingheau@gmail.com>
2024-07-31 12:15:21 +0530

    feat:creates LibraryCatalogue.ts class with singelton pattern

--- src/LibraryCatalogue.ts
+++ src/LibraryCatalogue.ts
@@ -2,7 +2,17 @@ import {Book} from "./Book";
 
 
 export class LibraryCatalogue{
+    private static instance: LibraryCatalogue;
     private libraryItems: Book[] = [];
+
+    private constructor() {}
+
+    public static getInstance(): LibraryCatalogue {
+        if(!LibraryCatalogue.instance){
+            LibraryCatalogue.instance = new LibraryCatalogue();
+        }
+        return LibraryCatalogue.instance;
+    }
 
     public addItem(item: Book):void{
         this.libraryItems.push(item);
